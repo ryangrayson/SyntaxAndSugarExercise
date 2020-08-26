2836e6830870fc737c9199e4d3dced59103e8ac7
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-26 08:41:35 -0500

    Exercise Completed

---
+++ SyntaxAndSugarExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SyntaxAndSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int answer = 4;
+            // string response;
+
+            //if (answer < 9)
+            //{
+            //response = answer + " is less than nine";
+            //  Console.WriteLine();
+            //}
+            //else
+            //{
+            //response = answer + “greater than or equal to nine”;
+            //}
+
+            var answer = 4;
+
+            var response = (answer < 9) ? ($"{answer} is less than nine") : ($"{answer} is greater than or equal to nine");
+
+            Console.WriteLine(response);
+
+
+
+
+        }
+    }
+}
